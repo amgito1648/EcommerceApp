9204f12f52db325bc8ed09a9cebe910466bb6c77
Cristian Cala <ccala25@unab.edu.co>
2025-04-09 08:51:10 -0500

    Diseño Login Screen

--- app/src/main/java/com/cristiancala/ecommerceapp/LoginScreen.kt
+++ app/src/main/java/com/cristiancala/ecommerceapp/LoginScreen.kt
@@ -1,6 +1,7 @@
 package com.cristiancala.ecommerceapp
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
@@ -11,10 +12,13 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Email
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Icon
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -26,16 +30,21 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
 @Composable
-fun LoginScreen(){
+fun LoginScreen() {
     Scaffold { innerPadding ->
-        Column (
-            modifier = Modifier.padding(innerPadding).fillMaxSize().padding(horizontal = 30.dp),
-            horizontalAlignment = Alignment.CenterHorizontally
-            ){
-            Image(painter = painterResource(R.drawable.logo_unab),
+        Column(
+            modifier = Modifier
+                .padding(innerPadding)
+                .fillMaxSize()
+                .padding(horizontal = 30.dp),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ) {
+            Image(
+                painter = painterResource(R.drawable.logo_unab),
                 contentDescription = "logo Unab",
                 modifier = Modifier.size(150.dp)
-                )
+            )
 
             Spacer(modifier = Modifier.height(32.dp))
 
@@ -69,13 +78,54 @@ fun LoginScreen(){
 
 
             )
+            Spacer(modifier = Modifier.height(24.dp))
 
+
+            OutlinedTextField(
+                value = "",
+                onValueChange = {},
+                modifier = Modifier.fillMaxWidth(),
+                leadingIcon = {
+                    Icon(
+                        imageVector = Icons.Default.Email,
+                        contentDescription = "Email",
+                        tint = Color(0xFFFF9900)
+                    )
+                },
+                label = {
+                    Text(text = "Contraseña")
+                },
+                shape = RoundedCornerShape(12.dp)
+            )
+
+            Button(
+                onClick = {}, modifier = Modifier
+                    .fillMaxWidth()
+                    .height(50.dp),
+                shape = RoundedCornerShape(12.dp),
+                colors = ButtonDefaults.buttonColors(
+                    containerColor = Color (0xFFFF9900)
+                )
+
+            ) {
+                Text("Iniciar Sesion")
+            }
+
+            Spacer(modifier = Modifier.height(24.dp))
+
+            TextButton(onClick = {}) {
+                Text(
+                    text = "¿No tienes Cuenta?, Registrate",
+                    color = Color(0xFFFF9900)
+                )
+            }
 
         }
     }
 }
+
 @Preview
 @Composable
-fun LoginScreenPreview(){
+fun LoginScreenPreview() {
     LoginScreen()
 }
